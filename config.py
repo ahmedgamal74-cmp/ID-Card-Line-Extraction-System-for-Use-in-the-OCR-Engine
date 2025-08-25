346f0e49de4d06df53145dafd34a80d9705433b3
Ahmed Gamal <Ahmed.Noureddine02@eng-st.cu.edu.eg>
2025-08-25 23:29:10 +0300

    main config commit

---
+++ config.py
@@ -0,0 +1,27 @@
+# ROIs extract using: "classic" for classical method
+#                     "det"     for detection model 
+#                     "seg"     for segmenation model
+option = "seg"
+
+# detection_model: "detr" for RT-DETR
+#                  "yolo" for YOLO
+detection_model = "yolo"
+
+# rectify
+rectify_input_dir   = "0_IDs_input/"
+rectify_output_dir  = "1_IDs_rectified/"
+
+# derotation
+derotation_ref_dir  = "0_IDs_derotation_ref/"
+derotated_dir       = "2_IDs_derotated/"
+
+# classical method
+classic_out_dir     = "3_ROIs_classic"
+rois_json           = "ROIs.json"
+crops_output_fromat = "png"
+
+# detect method
+det_out_dir = "3_ROIs_det"
+
+# segment method
+seg_out_dir = "3_ROIs_seg"
---
+++ main.py
@@ -0,0 +1,31 @@
+# simple main file to run the piepline
+import sys, subprocess
+from pathlib import Path
+from config import *
+
+# rectify
+print("Rectification started...")
+Path(rectify_output_dir).mkdir(parents=True, exist_ok=True)
+subprocess.run([sys.executable, "rectify.py", rectify_input_dir, rectify_output_dir], check=True)
+
+# derotate
+print("\nDerotatation started...")
+Path(derotated_dir).mkdir(parents=True, exist_ok=True)
+subprocess.run([sys.executable, "corr.py", "--ref_dir", derotation_ref_dir, 
+                                "--img_dir", rectify_output_dir, "--save_dir", derotated_dir], check=True)
+
+# extraction mode (classic/detection/segmentation)
+if option == "det":
+    print(f"\nCrops (ROIs) extraction using {detection_model} detection model started...")
+    Path(det_out_dir).mkdir(parents=True, exist_ok=True)
+    subprocess.run([sys.executable, "infer_det.py", "--input_dir", derotated_dir, "--save_dir", det_out_dir], check=True)
+elif option == "seg":
+    print(f"\nCrops (ROIs) extraction using yolo segmentation model started...")
+    Path(seg_out_dir).mkdir(parents=True, exist_ok=True)
+    subprocess.run([sys.executable, "infer_seg.py", "--input_dir", derotated_dir, "--save_dir", seg_out_dir], check=True)
+else:
+    print(f"\nCrops (ROIs) extraction using classical method started...")
+    Path(classic_out_dir).mkdir(parents=True, exist_ok=True)
+    subprocess.run([sys.executable, "roi_extract.py", derotated_dir, rois_json, 
+                                    "--outdir", classic_out_dir, "--ext", crops_output_fromat], check=True)
+
